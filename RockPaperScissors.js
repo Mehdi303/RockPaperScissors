ad65f288302bf6826e839f6bba24a500cba8ce15
Mehdi303 <malibaco12@gmail.com>
2025-05-20 18:45:13 +0200

    Finished Computer choice

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -1,4 +1,18 @@
+function getComputerChoice(){
+
 function getMathInteger() {
   return Math.floor(Math.random() * 3);
 }
-console.log(getMathInteger())
+const randomNumber= getMathInteger()
+
+if (randomNumber===0) {
+    return("Rock!")
+}
+else if (randomNumber===1) {
+    return("Paper!")
+}
+else {
+    return ("Scissors!")
+}
+}
+console.log(getComputerChoice())
